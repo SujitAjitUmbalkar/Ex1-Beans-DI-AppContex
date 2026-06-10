c833458795b0c475669cf32c0b4871a0acfbbdc8
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2026-06-10 23:19:39 +0530

    Scopes added

--- BeansDiIoc/src/main/java/com/codingshuttle/BeansDiIoc/PaymentService.java
+++ BeansDiIoc/src/main/java/com/codingshuttle/BeansDiIoc/PaymentService.java
@@ -2,13 +2,21 @@ package com.codingshuttle.BeansDiIoc;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 @Component
 //@Service
 //@Controller
 //@Repository
 //@RestController
+
+//@Scope("singleton")
+//@Scope("request")
+//@Scope("application")
+//@Scope("session")
+@Scope("prototype")
 public class PaymentService
 {
     public void  pay()
